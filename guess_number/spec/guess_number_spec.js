f283e2ed0ed2429e655f3b6b889ea603d5dd1f54
John Doe <johndoe@example.com>
2014-04-14 11:12:25 +0800

    create a random not repeatable four fingures

--- guess_number/spec/guess_number_spec.js
+++ guess_number/spec/guess_number_spec.js
@@ -9,10 +9,16 @@ describe("Number", function () {
 
     })
 
-    it("should create a random four figures", function () {
-        var number = Number.create()
+    it("should create a random not repeatable four figures", function () {
+        var number = Number.create();
+console.log(number,'n')
+        var a = parseInt(number / 1000);
+        var b = parseInt((number-a*1000) / 100);
+        var c = parseInt((number-a*1000-b*100) / 10);
+        var d = number % 10;
 
-        expect(999<number<10000).toBe(true);
+        expect(number > 999 && number < 10000).toBe(true);
+        expect(a != b && a != c && a != d && b != c && b != d && c != d).toBe(true);
+
     });
-
 });
