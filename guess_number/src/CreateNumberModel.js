5d5531c0e9937106fb68d21b9baa86a4349c3e01
John Doe <johndoe@example.com>
2014-04-15 17:34:33 +0800

    guess_number move to SpecRunner.html

---
+++ guess_number/src/CreateNumberModel.js
@@ -0,0 +1,22 @@
+function Number(){
+}
+
+Number.create = function(){
+    var a = parseInt(Math.random()*9 + 1);
+    var b = Number.create_0_9();
+    var c = Number.create_0_9();
+    var d = Number.create_0_9();
+    var not_repeat = Number.is_not_repeat(a,b,c,d)
+    if(not_repeat){
+        return a*1000+b*100+c*10+d;
+    }
+    return Number.create();
+}
+
+Number.create_0_9 = function(){
+    return parseInt(Math.random()*10);
+}
+
+Number.is_not_repeat = function(a,b,c,d){
+    return a != b && a != c && a != d && b != c && b != d && c != d;
+}
--- guess_number/src/GuessNumberModel.js
+++ guess_number/src/GuessNumberModel.js
@@ -1,22 +1,10 @@
-function Number(){
+function GuessNumber(){
+
 }
 
-Number.create = function(){
-    var a = parseInt(Math.random()*9 + 1);
-    var b = Number.create_0_9();
-    var c = Number.create_0_9();
-    var d = Number.create_0_9();
-    var not_repeat = Number.is_not_repeat(a,b,c,d)
-    if(not_repeat){
-        return a*1000+b*100+c*10+d;
+GuessNumber.result = function(){
+    var number1 = Number.create();
+    var number2 = document.getElementById('num').value;
+    var result = Judge.judge_type(number1,number2);
+    return result;
     }
-    return Number.create();
-}
-
-Number.create_0_9 = function(){
-    return parseInt(Math.random()*10);
-}
-
-Number.is_not_repeat = function(a,b,c,d){
-    return a != b && a != c && a != d && b != c && b != d && c != d;
-}
