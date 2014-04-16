60f9290f6d21cc16ec9118be9990910b7233e3d2
John Doe <johndoe@example.com>
2014-04-16 10:37:48 +0800

    set input is disabled when start

--- guess_number/src/GuessNumberModel.js
+++ guess_number/src/GuessNumberModel.js
@@ -5,16 +5,17 @@ var count = 6;
 
 GuessNumber.start = function(){
     if(count != 0){
-        document.getElementById('num').value = '';
+        document.getElementById('num').disabled = false;
         count--;
     }
-
 }
 
 GuessNumber.result = function(){
     var number1 = Number.create();
     var number2 = document.getElementById('num').value;
     var result = Judge.judge_type(number1,number2);
+    document.getElementById('num').value = '';
+    document.getElementById('num').disabled = true;
     if(count == 0){
         document.getElementById('start').disabled = 'disabled';
     }
