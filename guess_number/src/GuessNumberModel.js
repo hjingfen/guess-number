95178e99f70e62f75115d14e02dc4956f6f604ae
John Doe <johndoe@example.com>
2014-04-16 09:53:03 +0800

    set the count of guess

--- guess_number/src/GuessNumberModel.js
+++ guess_number/src/GuessNumberModel.js
@@ -1,10 +1,22 @@
 function GuessNumber(){
 
 }
+var count = 6;
+
+GuessNumber.start = function(){
+    if(count != 0){
+        document.getElementById('num').value = '';
+        count--;
+    }
+
+}
 
 GuessNumber.result = function(){
     var number1 = Number.create();
     var number2 = document.getElementById('num').value;
     var result = Judge.judge_type(number1,number2);
-    return result;
+    if(count == 0){
+        document.getElementById('start').disabled = 'disabled';
     }
+    return result;
+}
