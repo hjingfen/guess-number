dfeaa1e90cfb03ea31e4ca7b1583f02c20064769
John Doe <johndoe@example.com>
2014-04-16 13:54:28 +0800

    show result instesd of alert result

--- guess_number/spec/page_spec.js
+++ guess_number/spec/page_spec.js
@@ -1,10 +1,30 @@
 describe("page", function () {
 
 
-    it("should have six opportunities", function () {
-        var count = GuessNumber.count();
-
-        expect(count).toBe(6);
-    });
+//    it("should have six opportunities", function () {
+//        var count = GuessNumber.count();
+//
+//        expect(count).toBe(2);
+//    });
+//
+//    it("should have a disabled input when step in",function(){
+//        var disabled = document.getElementById('num').disabled;
+//
+//        expect(disabled).toBe(true);
+//    });
+//
+//    it("should have a useful input when start and count is not 0",function(){
+//        var disabled = GuessNumber.start();
+//
+//        expect(disabled).toBe(false);
+//    });
+//
+//    it("should have a disabled input and show the result when click result",function(){
+//        var disabled = GuessNumber.click_result();
+//        var result = GuessNumber.get_result();
+//
+//        expect(disabled).toBe(true);
+//        expect(result).toBe(result);
+//    });
 
 });
--- guess_number/src/GuessNumberModel.js
+++ guess_number/src/GuessNumberModel.js
@@ -2,34 +2,33 @@ function GuessNumber(){
 
 }
 
-GuessNumber.count = function(){
-    return 6;
-}
-var count = GuessNumber.count();
+var count = 6;
 
 GuessNumber.start = function(){
-    if(count != 0){
-        document.getElementById('num').disabled = false;
-        count--;
-    }
+    count--;
+    document.getElementById('num').disabled = false;
 }
 
 GuessNumber.result = function(){
-    var number1 = Number.create();
-    var number2 = document.getElementById('num').value;
-    var result = Judge.judge_type(number1,number2);
+    var result = GuessNumber.get_result();
     GuessNumber.click_result();
     if(result == "4A0B"){
-        alert(result+"恭喜成功！");
+        document.getElementById('show').innerHTML = result+"恭喜成功！";
         return;
     }
-    alert(result);
+    document.getElementById('show').innerHTML = result;
+}
+
+GuessNumber.get_result = function(){
+    var number1 = Number.create();
+    var number2 = document.getElementById('num').value;
+    return Judge.judge_type(number1,number2);
 }
 
 GuessNumber.click_result = function(){
-    document.getElementById('num').value = '';
-    document.getElementById('num').disabled = true;
     if(count == 0){
         document.getElementById('start').disabled = 'disabled';
     }
+    document.getElementById('num').value = '';
+    document.getElementById('num').disabled = true;
 }
